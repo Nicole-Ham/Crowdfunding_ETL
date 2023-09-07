edbf09cd46702c8a2d0ddae1f45cb8e05e1a610d
Nicole-Ham <NLHammack@gmail.com>
2023-09-07 13:06:28 -0700

    final touches

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Crowdfunding_ETL
 Module 13 Project
+
+Completed Solo
+
+Received help from tutor, Mark F., instructor, Mohan F., and TA Daniel G. 
+
+The instructions for this mini project are divided into the following subsections:
+
+Create the Category and Subcategory DataFrames
+Create the Campaign DataFrame
+Create the Contacts DataFrame
+Create the Crowdfunding Database
+    - querys are saved as screenshots
---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,59 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" varchar   NOT NULL,
+    "descriptions" varchar   NOT NULL,
+    "goal" decimal   NOT NULL,
+    "pledged" decimal   NOT NULL,
+    "outcome" varchar   NOT NULL,
+    "backer_count" int   NOT NULL,
+    "country" varchar   NOT NULL,
+    "currency" varchar   NOT NULL,
+    "launched_dates" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" varchar   NOT NULL,
+    "subcategory_id" varchar   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" varchar   NOT NULL,
+    "category" varchar   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" varchar   NOT NULL,
+    "subcategory" varchar   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+    "email" varchar   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
